37db7f46c51ed1ece141b08c9905a7a9d0186c8a
liweiyang91 <36722588+liweiyang91@users.noreply.github.com>
2018-02-23 11:09:19 +0800

    ios-8859-1 to utf-8
    原代码使用ios--8859-1编码，改为用utf-8编码

--- day01/code/01_HelloWorld案例/HelloWorld.java
+++ day01/code/01_HelloWorld案例/HelloWorld.java
@@ -1,6 +1,6 @@
 class HelloWorld {
 	public static void main(String[] args) {
 		//System.out.println("ÎÒ°®ÁÖÇàÏ¼");
-		System.out.println("Hello World");
+		System.out.println("Hello World"); 
 	}
 }
